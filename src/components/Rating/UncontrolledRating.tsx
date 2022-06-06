1a81ca1c99ff90abc545431f2a16d9ae220785eb
Evgenii <emeleshkov@mail.ru>
2022-06-06 04:04:46 +0300

    07.tasks done (ternary expression added in 'Star')

--- src/components/Rating/UncontrolledRating.tsx
+++ src/components/Rating/UncontrolledRating.tsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 
 
-
 export function UncontrolledRating() {
     console.log('UncontrolledRating rendering')
 
@@ -12,36 +11,28 @@ export function UncontrolledRating() {
 
     return (
         <div className="span">
-            <Star starsValue={starsValue} num={1} selected={ star > 0}/>
-            <Star starsValue={starsValue} num={2} selected={ star > 1}/>
-            <Star starsValue={starsValue} num={3} selected={ star > 2}/>
-            <Star starsValue={starsValue} num={4} selected={ star > 3}/>
-            <Star starsValue={starsValue} num={5} selected={ star > 4}/>
+            <Star starsValue={starsValue} num={1} selected={star > 0}/>
+            <Star starsValue={starsValue} num={2} selected={star > 1}/>
+            <Star starsValue={starsValue} num={3} selected={star > 2}/>
+            <Star starsValue={starsValue} num={4} selected={star > 3}/>
+            <Star starsValue={starsValue} num={5} selected={star > 4}/>
         </div>
     )
 }
 
 type StarPropType = {
     selected: boolean
-    starsValue: (num: number)=>void
+    starsValue: (num: number) => void
     num: number
 }
 
 function Star(props: StarPropType) {
-
-    if (props.selected === true) {
-        console.log('Star selected rendering')
-        return (
-            <span onClick={()=>props.starsValue(props.num)}>
+    return (
+        props.selected === true
+            ? (<span onClick={() => props.starsValue(props.num)}>
             <b> star </b>
-        </span>
-        )
-    } else {
-        console.log('Star unselected rendering')
-        return (
-            <span onClick={()=>props.starsValue(props.num)}> star </span>
-        )
-    }
-
+        </span>)
+            : (<span onClick={() => props.starsValue(props.num)}> star </span>)
+    )
 }
 
